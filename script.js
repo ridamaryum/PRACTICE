30dc4ae69003f3d46741a05f5304a916527f80a1
ridamaryum <maryumtahir67@gmail.com>
2025-05-09 23:40:59 +0500

    chnged cart quantity

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
-console.log(`hello friend`)
-console.log(2+2)
-console.log(`good morning` + `rida`)
-console.warn(`this is warning`);
+/*
+let bagPrice = (1500-(220+300)-20);
+let jeansPrice = (3000-(330*2)-110);
+let packingPrice = 250;
+let bagValue = bagPrice + jeansPrice + packingPrice * 18/100;
+let bagSummary = `your final myntra bag amount is Rs${bagValue}`;
+console.log(bagSummary);
+
+let a = 10;
+let b = 5;
+console.log(a-b);
+
+let noOfStudents = 20;
+noOfStudents = noOfStudents + 5;
+console.log(noOfStudents);*/
+
+let cartQuantity = 11;
+document.querySelector('#cart-summary').innerText = 'YOUR BAG HAS ' + cartQuantity + ' ITEMS';
